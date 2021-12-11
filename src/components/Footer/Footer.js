baefbcb20c42621ac4136d4df4f335ccc9bd0657
percy cordova <cordova_inf@hotmail.com>
2021-12-10 22:38:21 -0500

    .

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="flex justify-around p-10">
-      <div className="text-gray-700 p-3 text-xl">Contacto</div>
+    <footer className="flex  p-10">
+      {/* <div className="text-gray-700 p-3 text-xl">Contacto</div>
       <div className="text-gray-700 p-3 text-xl">Ciudad-Pais</div>
-      <div className="text-gray-700 p-3 text-xl">Direccion-Telefono</div>
+      <div className="text-gray-700 p-3 text-xl">Direccion-Telefono</div> */}
 
     </footer>
   )
